1665124f24b6cb3a1cc648aa9465a44028596d6a
AlenaKiporuk <alenakiporyk@gmail.com>
2022-11-14 04:36:44 +0200

    ready task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const galleryEl = document.querySelector('.gallery');
+const cardsMarkup = createImgCardMarkup(galleryItems);
+galleryEl.insertAdjacentHTML('beforeend', cardsMarkup);
+galleryEl.addEventListener('click', onGalleryElClick)
+function createImgCardMarkup( galleryItems ) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+<a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+        `;
+
+    }).join('');
+
+}
+function onGalleryElClick(e) {
+       e.preventDefault();
+ 
+    if (!e.target.classList.contains('gallery__image')) {
+        return;
+    }
+ const instance = basicLightbox.create(`
+		 <img src="${e.target.dataset.source}" width="1280">
+`)
+	instance.show();
+
+
+};
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
 console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+const cardsMarkup = createImgCardMarkup(galleryItems);
+galleryEl.insertAdjacentHTML('beforeend', cardsMarkup);
+galleryEl.addEventListener('click', onGalleryElClick)
+function createImgCardMarkup( galleryItems ) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+        `;
+
+    }).join('');
+
+}
+function onGalleryElClick(e) {
+       e.preventDefault();
+ 
+    if (!e.target.classList.contains('gallery__image')) {
+        return;
+    }
+
+};
+new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: "alt",
+    captionDelay: 250,
+    fadeSpeed: 250,
+   
+ });
+
